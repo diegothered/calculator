9f4131420fb60ca145545f995a01df26b5b9281d
Diego Rojas and Vilija Jozaitis <diego@>
2019-05-15 20:26:38 -0700

    built calculator.html with javascript to make calculations.

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+var result = add(number1, number2);
+alert(result);
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+var result = subtract(number1, number2);
+alert(result);
+
+var multiplication = function(number1, number2) {
+  return number1 * number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+var result = multiplication(number1, number2);
+alert(result);
+
+var division = function(number1, number2) {
+  return number1 / number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+var result = division(number1, number2);
+alert(result);
